bd0ad942f7c1719aa4a27c3412283c3937b8856f
LatellaL <83371530+LatellaL@users.noreply.github.com>
2022-04-16 18:42:43 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,6 @@ class StudentsController < ApplicationController
   def new
     @student = Student.new
   end
-
   def create
     @student = Student.new(student_params)
     if @student.save
@@ -11,20 +10,29 @@ class StudentsController < ApplicationController
       render 'new'
     end
   end
-
   def edit
     @student = Student.find(params[:id])
   end
-
   def show
     @student = Student.find(params[:id])
   end
 
   def index
-    @students = Student.all
+    @students = Student.search(params[:query])
+    render 'index'
   end
 
   def student_params
+
+    
+          
+            
+    
+
+          
+    
+    
+  
     params.require(:student).permit(:name, :birthday, :hometown)
   end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -9,8 +9,11 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query)
+    self.where('name LIKE ?', "%#{query}%")
+  end 
 end
